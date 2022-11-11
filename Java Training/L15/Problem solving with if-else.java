0150e38f397a343a2bed2d689074256be12bab12
Sachin Kumar <110603659+iamsacstar@users.noreply.github.com>
2022-11-11 12:15:57 +0530

    Create Problem solving with if-else.java

---
+++ Java Training/L15/Problem solving with if-else.java
@@ -0,0 +1,12 @@
+package q10844;
+public class IsWorkingDay {
+	public static void main(String[] args) {
+		//Fill in the missing code using if-else statement
+		if (args[0].equals("Sunday")) {
+			System.out.println("Holiday");
+		}
+		else{
+			System.out.println("Working Day");
+		}
+	}
+}
